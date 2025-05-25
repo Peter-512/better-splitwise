6e4b50bbd425663681a81a3e7a6035b4cbf1b7c8
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2025-05-25 14:47:41 +0200

    add field to table

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -2,5 +2,6 @@ import { pgTable, serial, text, integer } from 'drizzle-orm/pg-core';
 
 export const user = pgTable('user', {
 	id: serial('id').primaryKey(),
-	age: integer('age')
+	age: integer('age'),
+	name: text('name')
 });
