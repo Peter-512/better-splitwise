49400b60c88c336e6ec29d2ee12c88645cd2899e
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2025-05-25 18:03:54 +0200

    add birthday

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,7 +1,8 @@
-import { pgTable, serial, integer, text } from 'drizzle-orm/pg-core';
+import { pgTable, serial, integer, text, date } from 'drizzle-orm/pg-core';
 
 export const user = pgTable('user', {
 	id: serial('id').primaryKey(),
 	age: integer('age'),
-	name: text('name')
+	name: text('name'),
+	birthday: date('birthday')
 });
