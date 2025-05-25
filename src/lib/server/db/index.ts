4a1bff91e3ae7791278360e36862a15c00576750
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2025-05-25 14:35:59 +0200

    initial commit

---
+++ src/lib/server/db/index.ts
@@ -0,0 +1,8 @@
+import { drizzle } from 'drizzle-orm/neon-http';
+import { neon } from '@neondatabase/serverless';
+import { DATABASE_URL } from '$env/static/private';
+
+if (!DATABASE_URL) throw new Error('DATABASE_URL is not set');
+const client = neon(DATABASE_URL);
+export const db
+	= drizzle(client);
